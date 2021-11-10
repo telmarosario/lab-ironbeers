26917d816315b6e986c7503be8be31b5a057a0b2
Telma Rosário <telmarrosario@gmail.com>
2021-11-10 17:43:09 +0000

    done

--- app.js
+++ app.js
@@ -13,13 +13,32 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+//* Iteration 3 - Beers page
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersArray => {
+      //console.log(beersArray);  Array of objects. Each object is a beer with an id, name, tagline, image_url...
+      res.render('beers', { beersKey: beersArray }); // We need to call render here because the promise gets resolved here, if we call it outside it is a pending promise.
+    })
+    .catch(err => console.log(err));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(oneBeerArray => {
+      res.render('random-beer', { aBeerKey: oneBeerArray });
+    })
+    .catch(err => console.log(err));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
